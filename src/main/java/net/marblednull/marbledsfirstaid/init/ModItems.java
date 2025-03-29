365279dd115ef9907d8d8d3393a7908728e342fc
MarbledNull <cyandoezstuff@gmail.com>
2025-03-29 17:36:00 -0500

    porting

--- src/main/java/net/marblednull/marbledsfirstaid/init/ModItems.java
+++ src/main/java/net/marblednull/marbledsfirstaid/init/ModItems.java
@@ -16,14 +16,17 @@ public class ModItems {
     public static final DeferredItem<Item> CLOTH = ITEMS.registerItem("cloth", Item::new, new Item.Properties());
     public static final DeferredItem<Item> MEDICINE_BOTTLE = ITEMS.registerItem("medicine_bottle", Item::new, new Item.Properties());
     public static final DeferredItem<Item> AWKWARD_MEDICINE_BOTTLE = ITEMS.registerItem("awkward_medicine_bottle", Item::new, new Item.Properties());
+    //first aid items
     public static final DeferredItem<Item> MEDKIT = ITEMS.registerItem("medkit", MedkitItem::new, new Item.Properties().stacksTo(1));
     public static final DeferredItem<Item> BANDAGES = ITEMS.registerItem("bandages", BandagesItem::new, new Item.Properties().stacksTo(16));
     public static final DeferredItem<Item> BANDAID = ITEMS.registerItem("bandaid", BandaidItem::new, new Item.Properties().stacksTo(16));
     public static final DeferredItem<Item> PAIN_PILLS = ITEMS.registerItem("pain_pills", PainPillsItem::new, new Item.Properties().stacksTo(16));
+    //medicines
     public static final DeferredItem<Item> MORPHINE = ITEMS.registerItem("morphine", MorphineItem::new, new Item.Properties().food(new FoodProperties.Builder().alwaysEdible().effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 600, 0, false, false, false), 1.0f).effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 600, 0, false, false, false), 1.0f).build()));
     public static final DeferredItem<Item> EYE_DROPS = ITEMS.registerItem("eye_drops", EyeDropsItem::new, new Item.Properties().food(new FoodProperties.Builder().build()));
     public static final DeferredItem<Item> TONIC = ITEMS.registerItem("tonic", TonicItem::new, new Item.Properties().food(new FoodProperties.Builder().build()));
     public static final DeferredItem<Item> ANTIDOTE = ITEMS.registerItem("antidote", AntidoteItem::new, new Item.Properties().food(new FoodProperties.Builder().build()));
+    //syringes
     public static final DeferredItem<Item> STIMPACK = ITEMS.registerItem("stimpack", StimpackItem::new, new Item.Properties().food(new FoodProperties.Builder().build()));
     public static final DeferredItem<Item> ADRENALINE_SYRINGE = ITEMS.registerItem("adrenaline_syringe", AdrenalinesSyringeItem::new, new Item.Properties());
 
