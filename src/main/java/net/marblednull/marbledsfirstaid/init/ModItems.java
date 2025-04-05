301004cc9e2b82001689cb76e72f8190620ded66
MarbledNull <cyandoezstuff@gmail.com>
2025-04-05 12:18:28 -0500

    fixed item cooldowns

--- src/main/java/net/marblednull/marbledsfirstaid/init/ModItems.java
+++ src/main/java/net/marblednull/marbledsfirstaid/init/ModItems.java
@@ -24,10 +24,10 @@ public class ModItems {
     public static final DeferredItem<Item> SYRINGE = ITEMS.registerItem("syringe", Item::new, new Item.Properties());
     public static final DeferredItem<Item> AWKWARD_SYRINGE = ITEMS.registerItem("awkward_syringe", Item::new, new Item.Properties());
     //first aid items
-    public static final DeferredItem<Item> MEDKIT = ITEMS.registerItem("medkit", MedkitItem::new, new Item.Properties().stacksTo(1).useCooldown(5f));
-    public static final DeferredItem<Item> BANDAGES = ITEMS.registerItem("bandages", BandagesItem::new, new Item.Properties().stacksTo(16));
-    public static final DeferredItem<Item> BANDAID = ITEMS.registerItem("bandaid", BandaidItem::new, new Item.Properties().stacksTo(16));
-    public static final DeferredItem<Item> PAIN_PILLS = ITEMS.registerItem("pain_pills", PainPillsItem::new, new Item.Properties().stacksTo(16));
+    public static final DeferredItem<Item> MEDKIT = ITEMS.registerItem("medkit", MedkitItem::new, new Item.Properties().stacksTo(1).useCooldown(10f));
+    public static final DeferredItem<Item> BANDAGES = ITEMS.registerItem("bandages", BandagesItem::new, new Item.Properties().stacksTo(16).useCooldown(5f));
+    public static final DeferredItem<Item> BANDAID = ITEMS.registerItem("bandaid", BandaidItem::new, new Item.Properties().stacksTo(16).useCooldown(1f));
+    public static final DeferredItem<Item> PAIN_PILLS = ITEMS.registerItem("pain_pills", PainPillsItem::new, new Item.Properties().stacksTo(16).useCooldown(2f));
     //medicines
     public static final DeferredItem<Item> MORPHINE = ITEMS.registerItem("morphine", MorphineItem::new, new Item.Properties().useCooldown(2).component(DataComponents.CONSUMABLE, Consumable.builder().sound(SoundEvents.HONEY_DRINK).soundAfterConsume(SoundEvents.HONEY_DRINK).animation(ItemUseAnimation.DRINK).onConsume(new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 600, 0, false, false, false), 1.0f)).onConsume(new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(MobEffects.REGENERATION, 600, 0, false, false, false), 1.0f)).build()));
     public static final DeferredItem<Item> EYE_DROPS = ITEMS.registerItem("eye_drops", EyeDropsItem::new, new Item.Properties().useCooldown(1));
@@ -35,9 +35,9 @@ public class ModItems {
     public static final DeferredItem<Item> ANTIDOTE = ITEMS.registerItem("antidote", AntidoteItem::new, new Item.Properties());
     public static final DeferredItem<Item> ELIXIR = ITEMS.registerItem("elixir", Item::new, new Item.Properties());
     //syringes
-    public static final DeferredItem<Item> STIMPACK = ITEMS.registerItem("stimpack", StimpackItem::new, new Item.Properties());
-    public static final DeferredItem<Item> ADRENALINE_SYRINGE = ITEMS.registerItem("adrenaline_syringe", AdrenalinesSyringeItem::new, new Item.Properties());
-    public static final DeferredItem<Item> MORPHINE_SYRINGE = ITEMS.registerItem("morphine_syringe", Item::new, new Item.Properties());
+    public static final DeferredItem<Item> STIMPACK = ITEMS.registerItem("stimpack", StimpackItem::new, new Item.Properties().useCooldown(5f));
+    public static final DeferredItem<Item> ADRENALINE_SYRINGE = ITEMS.registerItem("adrenaline_syringe", AdrenalineSyringeItem::new, new Item.Properties().useCooldown(5f));
+    public static final DeferredItem<Item> MORPHINE_SYRINGE = ITEMS.registerItem("morphine_syringe", MorphineSyringeItem::new, new Item.Properties().useCooldown(5f));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
