e8806c673acbae1611d198d21bfebd7e5461b1f1
MarbledNull <cyandoezstuff@gmail.com>
2025-04-10 18:27:59 -0500

    improvement

--- src/main/java/net/marblednull/marbledsfirstaid/event/ModSounds.java
+++ src/main/java/net/marblednull/marbledsfirstaid/event/ModSounds.java
@@ -1,5 +1,6 @@
 package net.marblednull.marbledsfirstaid.event;
 
+import net.marblednull.marbledsfirstaid.MarbledsFirstAid;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
@@ -9,28 +10,17 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModSounds {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
-            DeferredRegister.create(BuiltInRegistries.SOUND_EVENT, "marbledsfirstaid");
+            DeferredRegister.create(BuiltInRegistries.SOUND_EVENT, MarbledsFirstAid.MODID);
 
-    public static final DeferredHolder<SoundEvent, SoundEvent> MEDKIT = SOUND_EVENTS.register(
-            "medkit",
-            () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath("marbledsfirstaid", "medkit"))
-    );
-    public static final DeferredHolder<SoundEvent, SoundEvent> SYRINGE = SOUND_EVENTS.register(
-            "syringe",
-            () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath("marbledsfirstaid", "stim"))
-    );
-    public static final DeferredHolder<SoundEvent, SoundEvent> BANDAGES = SOUND_EVENTS.register(
-            "bandages",
-            () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath("marbledsfirstaid", "bandages"))
-    );
-    public static final DeferredHolder<SoundEvent, SoundEvent> EYE_DROPS = SOUND_EVENTS.register(
-            "eye_drops",
-            () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath("marbledsfirstaid", "eye_drops"))
-    );
-    public static final DeferredHolder<SoundEvent, SoundEvent> PAIN_PILLS = SOUND_EVENTS.register(
-            "pain_pills",
-            () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath("marbledsfirstaid", "pain_pills"))
-    );
+    public static final DeferredHolder<SoundEvent, SoundEvent> MEDKIT = registerSoundEvents("medkit");
+    public static final DeferredHolder<SoundEvent, SoundEvent> SYRINGE = registerSoundEvents("syringe");
+    public static final DeferredHolder<SoundEvent, SoundEvent> BANDAGES = registerSoundEvents("bandages");
+    public static final DeferredHolder<SoundEvent, SoundEvent> EYE_DROPS = registerSoundEvents("eye_drops");
+    public static final DeferredHolder<SoundEvent, SoundEvent> PAIN_PILLS = registerSoundEvents("pain_pills");
+
+    private static DeferredHolder<SoundEvent, SoundEvent> registerSoundEvents(String name) {
+        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(MarbledsFirstAid.MODID, name)));
+    }
 
     public static void register(IEventBus eventBus) {
         SOUND_EVENTS.register(eventBus);
